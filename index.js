3cff32b000b3511ef94b3f88c04472d71f91ebdf
Xavy San <francisco.sandoval23b@udgvirtual.udg.mx>
2024-07-19 09:43:02 -0600

    Implementación de BD 2

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const { autenticar, handleAuthCallback } = require('./auth'); // Importar las funciones de auth.js
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Middleware para manejar datos de formularios
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false, // Asegúrate de no guardar sesiones no inicializadas
+    cookie: { secure: false, httpOnly: true, maxAge: (30 * 24 * 60 * 60 * 1000) }
+}));
+
+// Middleware para imprimir el estado de la sesión
+app.use((req, res, next) => {
+    //console.log('Estado de la sesión:', req.session);
+    next();
+});
+
+
+app.get('/auth', handleAuthCallback);
+
+app.get('/dashboard', async (req, res) => {
+    const tokenid = await autenticar(req, res, '/dashboard');
+    if (tokenid) {
+        const profileid = req.session.profileid;
+        console.log(`Usuario:\n${profileid.nickname}`);
+        console.log(`Token:\n${tokenid}`);
+        res.sendFile(__dirname + '/public/dashboard.html');
+    }
+});
+
+app.get('/questions', async (req, res) => {
+    const tokenid = await autenticar(req, res, '/questions');
+    if (tokenid) {
+        res.sendFile(__dirname + '/public/questions.html');
+    }
+});
+
+
 // Nueva ruta para recibir notificaciones
 app.post('/callback', async (req, res) => {
     const notification = req.body;
@@ -47,4 +94,13 @@ app.post('/callback', async (req, res) => {
         console.error('Error processing notification:', error);
         res.status(500).send('Error processing notification');
     }
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
 });
